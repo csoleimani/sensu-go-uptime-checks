63702bf0a5aa3f43e0a43db48894452e6c1e3aae
Cameron Soleimani <csoleimani@uber.com>
2019-08-20 10:34:00 -0700

    Flag in days rather than time

--- main.go
+++ main.go
@@ -2,103 +2,106 @@ package main
 
 //Import the packages we need
 import (
-	"fmt"
-	"os"
-	"time"
-	"io"
+    "fmt"
+    "os"
+    "time"
+    "io"
 
-	"github.com/sensu/sensu-go/types"
-	"github.com/shirou/gopsutil/host"
-	"github.com/spf13/cobra"
+    "github.com/sensu/sensu-go/types"
+    "github.com/shirou/gopsutil/host"
+    "github.com/spf13/cobra"
 )
 
 //Set up some variables. Most notably, warning and critical as time durations
 var (
-	warning, critical time.Duration
-	stdin   *os.File
+    warning, critical int64 
+    stdin   *os.File
 )
 
 //Start our main function
 func main() {
-	rootCmd := configureRootCommand()
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
+    rootCmd := configureRootCommand()
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Fprintf(os.Stderr, "error: %v\n", err)
+        os.Exit(1)
+    }
 }
 
 //Set up our flags for the command. Note that we have time duration defaults for warning & critical
 func configureRootCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "sensu-go-uptime-status",
-		Short: "The Sensu Go check for system uptime",
-		RunE:  run,
-	}
+    cmd := &cobra.Command{
+        Use:   "sensu-go-uptime-status",
+        Short: "The Sensu Go check for system uptime",
+        RunE:  run,
+    }
 
-	cmd.Flags().DurationVarP(&warning,
-		"warning",
-		"w",
-		time.Duration(72*time.Hour),
-		"Warning value in seconds, minutes, or hours, default is 72 hours (72h)")
+    cmd.Flags().Int64VarP(&warning,
+        "warning",
+        "w",
+        60,
+        "Warning value in days. Default is 60")
 
-	cmd.Flags().DurationVarP(&critical,
-		"critical",
-		"c",
-		time.Duration(168*time.Hour),
-		"Critical value in seconds, minutes, or hours default is 1 week (168h)")
+    cmd.Flags().Int64VarP(&critical,
+        "critical",
+        "c",
+        90,
+        "Critical value in days. Default is 90")
 		
-	return cmd
+    return cmd
 }
 
 func run(cmd *cobra.Command, args []string) error {
 
-	if len(args) != 0 {
-		_ = cmd.Help()
-		return fmt.Errorf("invalid argument(s) received")
-	}
+    if len(args) != 0 {
+        _ = cmd.Help()
+        return fmt.Errorf("invalid argument(s) received")
+    }
 
-	if stdin == nil {
-		stdin = os.Stdin
-	}
+    if stdin == nil {
+        stdin = os.Stdin
+    }
 	
-	event := &types.Event{}
+    event := &types.Event{}
 	
-	return checkUptime(event)
+    return checkUptime(event)
 }
 
 //Here we start the meat of what we do.
 func checkUptime(event *types.Event) error {
 	
-	//Setting "CheckUptime" as a constant
-	const checkName = "CheckUptime"
-	const metricName = "current_system_uptime"
-	//Setting uptime as the value retrieved from gopsutil
-	uptime, err := host.Uptime()
+    //Setting "CheckUptime" as a constant
+    const checkName = "CheckUptime"
+    const metricName = "current_system_uptime"
+    //Setting uptime as the value retrieved from gopsutil
+    uptime, err := host.Uptime()
 	
-	//Let's set up some error handling
-	if err != nil {
-		msg := fmt.Sprintf("Failed to determine uptime %s", err.Error())
-		io.WriteString(os.Stdout, msg)
-		os.Exit(3)
-	}
+    //Let's set up some error handling
+    if err != nil {
+        msg := fmt.Sprintf("Failed to determine uptime %s", err.Error())
+        io.WriteString(os.Stdout, msg)
+        os.Exit(3)
+    }
 	
-	//Add a variable for uptimeSecs, which converts uptime to a duration
-	uptimeSecs := time.Duration(uptime)*time.Second
-	
-	//Sets up conditionss for a comparison
-	if uptimeSecs > critical {
-		msg := fmt.Sprintf("%s CRITICAL - value = %d | %s=%d\n", checkName, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
-		io.WriteString(os.Stdout, msg)
-		os.Exit(2)
-	} else if uptimeSecs >= warning && uptimeSecs <= critical {
-		msg := fmt.Sprintf("%s WARNING - value = %d | %s=%d\n", checkName, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
-		io.WriteString(os.Stdout, msg)
-		os.Exit(1)
-	} else {
-		msg := fmt.Sprintf("%s OK - value = %d | %s=%d\n", checkName, int64(uptimeSecs.Seconds()), metricName, int64(uptimeSecs.Seconds()))
-		io.WriteString(os.Stdout, msg)
-		os.Exit(0)
-	}
-	return nil
+    //Add a variable for uptimeSecs, which converts uptime to a duration
+    uptimeSecs := time.Duration(uptime)*time.Second
+    uptimeDays := int64(uptimeSecs.Seconds())/86400
+    fmt.Sprintf("%d is uptime days",uptimeDays)
+    //Sets up conditionss for a comparison
+    if uptimeDays > critical {
+        msg := fmt.Sprintf("%s CRITICAL - value = %d days | %s=%d days\n", checkName, uptimeDays, metricName, uptimeDays)
+        io.WriteString(os.Stdout, msg)
+        os.Exit(2)
+    } else if uptimeDays >= warning && uptimeDays <= critical {
+        msg := fmt.Sprintf("%s WARNING - value = %d days | %s=%d days\n", checkName, uptimeDays, metricName, uptimeDays)
+        io.WriteString(os.Stdout, msg)
+        os.Exit(1)
+    } else {
+        msg := fmt.Sprintf("%s OK - value = %d days | %s=%d days\n", checkName, uptimeDays, metricName, uptimeDays)
+        io.WriteString(os.Stdout, msg)
+        os.Exit(0)
+    }
+    return nil
 }
+
+
 
